3a1c29fb4631a00fdf8012c588b21ce068a56a3d
Sugumar Raja <sugumarraja@blueehr.com>
2025-06-11 20:59:58 +0530

    jenkins test

--- src/test/java/sauce/qa/ecom/test/SauceHomePageTest.java
+++ src/test/java/sauce/qa/ecom/test/SauceHomePageTest.java
@@ -2,6 +2,6 @@ package sauce.qa.ecom.test;
 
 public class SauceHomePageTest {
 public void test() {
-	System.out.println("i am the new test addedx");
+	System.out.println("i am the new test added ss");
 }
 }
